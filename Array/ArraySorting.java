c1978ef10c5c03fcc6fde05037267535ac93cbef
nayansurve <survenayan870@gmail.com>
2025-11-10 21:49:11 +0530

    Array

--- Array/ArraySorting.java
+++ Array/ArraySorting.java
@@ -3,35 +3,34 @@ import java.util.Scanner;
 public class ArraySorting {
     public static void main(String args[]){
 
-    int arr[]=new int[5];
-    int i,j,temp;
-    Scanner sc=new Scanner(System.in);
-    System.out.println("Enter the Five Elements=");
+        int arr[] = new int[5];
+        int i, j, temp;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the Five Elements=");
 
-    for(i=0;i<5;i++)
-    {
-        arr[i]=sc.nextInt();
-    }
-    {
-        for(j=0;j<4;j++)
+        for(i = 0; i < 5; i++)
         {
-            if(arr[j]>arr[j+1])
-            {
-                temp=arr[j];
-                arr[j]=arr[j+1];
-                arr[j+1]=temp;
+            arr[i] = sc.nextInt();
+        }
 
+        // Bubble Sort
+        for(i = 0; i < 5; i++)          // outer loop
+        {
+            for(j = 0; j < 4 - i; j++)   // inner loop
+            {
+                if(arr[j] > arr[j+1])
+                {
+                    temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
             }
         }
+
         System.out.println("Sorted Elements are=");
+        for(i = 0; i < 5; i++)
         {
-            for(i=0;i<5;i++)
-            {
-                System.out.println(arr[i]);
-            }
+            System.out.println(arr[i]);
         }
     }
-
-    
-}
 }
--- Array/SortingArray.java
+++ Array/SortingArray.java
@@ -1,3 +1,35 @@
+import java.util.Scanner;
+
 public class SortingArray {
-    
+    public static void main(String args[])
+    {
+        int arr[]=new int[10];
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter an Array =");
+
+        for(int i=0;i<10;i++)
+        {
+            arr[i]=sc.nextInt();
+        }
+
+        // Bubble Sort
+        for(int i=0;i<10;i++)
+        {
+            for(int j=0;j<9-i;j++)
+            {
+                if(arr[j] > arr[j+1])
+                {
+                    int temp=arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=temp;
+                }
+            }
+        }
+
+        System.out.println("Sorted Elements are = ");
+        for(int i=0;i<10;i++)
+        {
+            System.out.println(arr[i]);
+        }
+    }
 }
