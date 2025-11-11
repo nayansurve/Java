4ef33085c456e1970010eb03ce04d297c972cf59
nayansurve <survenayan870@gmail.com>
2025-11-11 20:07:13 +0530

    Anagram

---
+++ BasicCoreJavaProgram/Anagram.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Anagram {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String str1,str2;
+        System.out.println("Enter a String");
+        str1=sc.nextLine();
+        System.out.println("Enter second string");
+        str2=sc.nextLine();
+    }
+}
