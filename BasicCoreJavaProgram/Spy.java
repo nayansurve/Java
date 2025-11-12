473ba3bb7225d557824d61d0d7c52b70d719a6b1
nayansurve <survenayan870@gmail.com>
2025-11-12 22:01:14 +0530

    SpyNumber

--- BasicCoreJavaProgram/Spy.java
+++ BasicCoreJavaProgram/Spy.java
@@ -1,3 +1,19 @@
+import java.util.Scanner;
+
 public class Spy {
+
+    public static void main(String args[])
+    {
+        int n,rem=0,mul=1;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter a number");
+        n=sc.nextInt();
+
+        while (n>0) {
+
+            rem=n%10;
+            
+        }
+    }
     
 }
