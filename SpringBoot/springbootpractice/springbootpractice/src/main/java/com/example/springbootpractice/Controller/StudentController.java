fcaa361c89e97723a539fb588048ab0f1c4555e1
nayansurve <survenayan870@gmail.com>
2025-11-14 23:49:24 +0530

    entity

--- SpringBoot/springbootpractice/springbootpractice/src/main/java/com/example/springbootpractice/Controller/StudentController.java
+++ SpringBoot/springbootpractice/springbootpractice/src/main/java/com/example/springbootpractice/Controller/StudentController.java
@@ -13,7 +13,7 @@ public class StudentController {
     }
 
     @PostMapping("/add")
-    public Student addStudent(@RequestBody Student student) {
+    public Student addStudent(@RequestBody Student student) 
         return student;   // return same data back
-    }
+    
 }
